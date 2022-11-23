d88e3d5589b1e21d15e3601b166d616eb67b1648
SearcherX <93086610+SearcherX@users.noreply.github.com>
2022-11-23 22:26:16 +0300

    Security boot

---
+++ src/main/java/learning/mvcstudentapplication/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package learning.mvcstudentapplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .antMatchers("/", "/students", "/webjars/**", "/css/**", "/img/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                )
+                .logout().logoutSuccessUrl("/");
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails root =
+                User.withDefaultPasswordEncoder()
+                        .username("root")
+                        .password("root")
+                        .roles("ADMIN")
+                        .build();
+
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("user")
+                        .roles("USER")
+                        .build();
+        return new InMemoryUserDetailsManager(root, user);
+    }
+
+}
