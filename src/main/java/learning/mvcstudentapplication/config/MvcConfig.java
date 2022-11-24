b6984cb8160d7bc3ef3e58d4d5f16632bef0c595
SearcherX <93086610+SearcherX@users.noreply.github.com>
2022-11-24 12:42:48 +0300

    Login-Logout btns

--- src/main/java/learning/mvcstudentapplication/config/MvcConfig.java
+++ src/main/java/learning/mvcstudentapplication/config/MvcConfig.java
@@ -5,7 +5,6 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-//@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
